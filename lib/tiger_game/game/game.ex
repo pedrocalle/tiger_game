370423ecc77803d161e10013c017091b782bed0c
Pedro Calle <ph.calle@hotmail.com>
2024-10-03 19:23:27 -0300

    Co-authored-by: Matheus de Camargo Marques <matheuscamarques@users.noreply.github.com>

--- lib/tiger_game/game/game.ex
+++ lib/tiger_game/game/game.ex
@@ -3,8 +3,31 @@ defmodule TigerGame.Game.Game do
   def generate_game() do
     [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     |> Enum.map(fn line ->
-      Enum.map(line, fn _num -> :rand.uniform(9) |> round() end)
+       line
+        Enum.map(line, fn _num -> :rand.uniform(5) |> round() end)
     end)
-    |>Validator.count_matches()
+    |>IO.inspect()
+    |>Validator.matches()
   end
+
+
+  ## se ele ta apostando se nao pontuar nada , ele perde o dinheiro
+  def apostar(amount) do
+    generate_game()
+    |> (fn game_state ->
+        score = Map.get(game_state, :score, -1)
+
+        game_state = game_state
+        |> Map.put(:amount, amount)
+        |> Map.put(:total_received,   score * amount)
+
+        Map.put(game_state , :winner, game_win?(game_state))
+
+      end).()
+  end
+
+  def game_win?(%{total_received: total_received} = _game) when total_received > 0, do: true
+
+  def game_win?(_) ,do: false
+
 end
--- lib/tiger_game/game/validator.ex
+++ lib/tiger_game/game/validator.ex
@@ -1,78 +1,106 @@
 defmodule TigerGame.Game.Validator do
-  def count_matches(board) do
-    0
-    |> check_match(fn -> validate_1(board) end)
-    |> check_match(fn -> validate_2(board) end)
-    |> check_match(fn -> validate_3(board) end)
-    |> check_match(fn -> validate_4(board) end)
-    |> check_match(fn -> validate_5(board) end)
-    |> check_match(fn -> validate_6(board) end)
-    |> check_match(fn -> validate_7(board) end)
-    |> check_match(fn -> validate_8(board) end)
+
+  @score_map %{
+      0 => 0.1,
+      1 => 0.2,
+      2 => 0.3,
+      3 => 0.4,
+      4 => 0.5,
+      5 => 0.6
+    }
+
+    def matches(board) do
+    %{
+        score: count_matches(0, board),
+        matches: count_matches([], board)
+     }
   end
 
-  defp check_match(count, func) do
-    cond do
-      func.() == true -> count + 1
-      func.() == false -> count
+  def count_matches(value, board) do
+    value
+    |> check_match(fn -> validate_line_1(board) end)
+    |> check_match(fn -> validate_line_2(board) end)
+    |> check_match(fn -> validate_line_3(board) end)
+    |> check_match(fn -> validate_column_1(board) end)
+    |> check_match(fn -> validate_column_2(board) end)
+    |> check_match(fn -> validate_column_3(board) end)
+    |> check_match(fn -> validate_diagonal_1(board) end)
+    |> check_match(fn -> validate_diagonal_2(board) end)
+  end
+
+  defp check_match(count, func) when is_number(count) do
+    result = func.()
+
+    case result  do
+       {x, _ponto} -> count + Map.get(@score_map, x, 1)
+       false -> count
     end
   end
 
-  defp validate_1([
+  defp check_match(count, func) when is_list(count) do
+    result = func.()
+
+    case result  do
+       {_x, ponto} -> [ponto | count]
+       false -> count
+    end
+  end
+
+  defp validate_line_1([
     [x, x, x],
     [_d, _e, _f],
     [_g, _h, _i]
-  ]), do: {true, x}
-  defp validate_1(_), do: false
+  ]), do: {x , "line_1"}
+  defp validate_line_1(_), do: false
 
-  defp validate_2([
+  defp validate_line_2([
     [_a, _b, _c],
     [x, x, x],
     [_g, _h, _i]
-  ]), do: {true, x}
-  defp validate_2(_), do: false
+  ]), do: {x , "line_2"}
+  defp validate_line_2(_), do: false
 
-  defp validate_3([
+  defp validate_line_3([
     [_a, _b, _c],
     [_d, _e, _f],
     [x, x, x]
-  ]), do: {true, x}
-  defp validate_3(_), do: false
+  ]), do: {x , "line_3"}
+  defp validate_line_3(_), do: false
 
-  defp validate_4([
+  defp validate_column_1([
     [x, _b, _c],
     [x, _e, _f],
     [x, _h, _i]
-  ]), do: {true, x}
-  defp validate_4(_), do: false
+  ]), do: {x , "column_1"}
+  defp validate_column_1(_), do: false
 
-  defp validate_5([
+  defp validate_column_2([
     [_a, x, _c],
     [_d, x, _f],
     [_g, x, _i]
-  ]), do: {true, x}
-  defp validate_5(_), do: false
+  ]), do: {x , "column_2"}
+  defp validate_column_2(_), do: false
 
-  defp validate_6([
+  defp validate_column_3([
     [_a, _b, x],
     [_d, _e, x],
     [_g, _h, x]
-  ]), do: {true, x}
-  defp validate_6(_), do: false
+  ]), do: {x , "column_3"}
+  defp validate_column_3(_), do: false
 
-  defp validate_7([
+  defp validate_diagonal_1([
     [x, _b, _c],
     [_d, x, _f],
     [_g, _h, x]
-  ]), do: {true, x}
-  defp validate_7(_), do: false
+  ]), do: {x, "diagonal_1"}
+  defp validate_diagonal_1(_), do: false
 
-  defp validate_8([
+  defp validate_diagonal_2([
     [_a, _b, x],
     [_d, x, _f],
     [x, _h, _i]
-  ]), do: {true, x}
-  defp validate_8(_), do: false
+  ]), do: {x, "diagonal_2"}
+  defp validate_diagonal_2(_), do: false
 
   def validate([
     [_a, _b, _c],
